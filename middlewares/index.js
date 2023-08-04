3c767c6e888221746965da8d91d0007f90105830
dthcs <dthcsc@gmail.com>
2023-08-04 14:53:07 +0900

    cors

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,7 @@
 const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const { Domain } = require('../models');
 
 exports.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()){
@@ -32,5 +35,37 @@ exports.verifyToken = (req, res, next) => {
             code: 401,
             message: "Invalid Token",
         });
+    }
+};
+
+exports.apiLimiter = rateLimit({
+    windowMs: 60*1000, //1 minute
+    max: 2,
+    handler(req, res){
+        res.status(this.statusCode).json({
+            code: this.statusCode,
+            message: "access within 1 minute only",
+        });
+    },
+});
+
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: 'new version issued. Please use new version',
+    });
+};
+
+exports.corsWhenDomainMatches = async(req, res, next) => {
+    const domain = await Domain.findOne({
+        where: { host: new URL(req.get('origin')).host },
+    });
+    if(domain){
+        cors({
+            origin: req.get('origin'),
+            credentials: true,
+        })(req, res, next);
+    }else{
+        next();
     }
 };
